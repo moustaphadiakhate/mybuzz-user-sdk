43356888456a656597147f8a71bc06255f430856
Moustapha <moustaphadiakhate1212@gmail.com>
2019-11-09 23:58:07 +0000

    00 SDK

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    path: __dirname,
+    filename: 'dist/index.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
